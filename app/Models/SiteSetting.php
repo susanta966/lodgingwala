3a00463aa96add843a7b83e075149235b9e47cd1
SUSANTA KUMAR BEHERA <156878395+susanta966@users.noreply.github.com>
2025-09-26 05:07:16 +0000

    sitesetting seeder for data insert2

--- app/Models/SiteSetting.php
+++ app/Models/SiteSetting.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class SiteSetting extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'site_title',
+        'logo',
+        'favicon',
+        'ftlogo',
+        'address',
+        'email',
+        'phone',
+        'whatsapp',
+        'instagram',
+        'twitter',
+        'facebook',
+        'og_type',
+        'og_title',
+        'og_description',
+        'og_url',
+        'og_site_name',
+        'og_image',
+        'og_width',
+        'og_height',
+        'twitter_card',
+        'twitter_title',
+        'twitter_image',
+        'site_about',
+        'site_location',
+    ];
 }
