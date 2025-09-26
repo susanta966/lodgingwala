db521313df253b251359d4fd4f88fe73b7873840
SUSANTA KUMAR BEHERA <156878395+susanta966@users.noreply.github.com>
2025-09-26 05:04:11 +0000

    fix amenties issue

--- database/migrations/2024_11_11_093941_create_banquets_table.php
+++ database/migrations/2024_11_11_093941_create_banquets_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('slug')->unique();
             $table->string('image');
             $table->string('person');
+            $table->json('amenities_id')->nullable();
             $table->timestamps();
         });
     }
